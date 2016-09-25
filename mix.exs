2aae83110b48c32efb97c2117c9c646d162b730a
Miles Z. Sterrett <miles@mileszs.com>
2016-09-25 14:20:14 -0400

    Add users, user authentication

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule ElixirJobBoard.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:ex_machina, "~> 1.0.2"}]
+     {:ex_machina, "~> 1.0.2"},
+     {:comeonin, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,11 @@ defmodule ElixirJobBoard.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    get "/login", SessionController, :new
+    post "/login", SessionController, :create
+    delete "/logout", SessionController, :delete
+
     resources "/posts", PostsController
   end
 
