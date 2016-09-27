2cb8b1b89921e30948f580bc772949271eb08709
Miles Z. Sterrett <miles@mileszs.com>
2016-09-26 22:53:19 -0400

    Add seeds for user accounts

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,25 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Empty the users table
+ElixirJobBoard.Repo.delete_all(ElixirJobBoard.User)
+
+default_password = "password"
+
+pg_changeset = ElixirJobBoard.User.changeset(
+  %ElixirJobBoard.User{},
+  %{ "email" => "pg13@pacers.com", "password" => "password" }
+)
+myles_changeset = ElixirJobBoard.User.changeset(
+  %ElixirJobBoard.User{},
+  %{ "email" => "myles@pacers.com", "password" => "password" }
+)
+zeisloft_changeset = ElixirJobBoard.User.changeset(
+  %ElixirJobBoard.User{},
+  %{ "email" => "nick.zeisloft.iu@pacers.com", "password" => "password" }
+)
+
+ElixirJobBoard.Registration.create(pg_changeset, ElixirJobBoard.Repo)
+ElixirJobBoard.Registration.create(myles_changeset, ElixirJobBoard.Repo)
+ElixirJobBoard.Registration.create(zeisloft_changeset, ElixirJobBoard.Repo)
