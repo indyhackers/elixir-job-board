14344f2831360c9bb251b8dbaac6f414b2e66d20
Miles Z. Sterrett <miles@mileszs.com>
2016-09-30 00:08:12 -0400

    Add test for registration

---
+++ test/models/registration_test.exs
@@ -0,0 +1,21 @@
+defmodule ElixirJobBoard.RegistrationTest do
+  use ElixirJobBoard.ModelCase
+
+  alias ElixirJobBoard.Registration
+  alias ElixirJobBoard.User
+
+  @valid_attrs %{ password: "some content", email: "some_content@some_content.com" }
+  @invalid_attrs %{ password: "some content" }
+
+  test "creates a user when given valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    result = Registration.create(changeset, ElixirJobBoard.Repo)
+    assert elem(result, 0) == :ok
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    result = Registration.create(changeset, ElixirJobBoard.Repo)
+    refute elem(result, 0) == :ok
+  end
+end
