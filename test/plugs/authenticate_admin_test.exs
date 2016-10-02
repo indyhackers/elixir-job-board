c4cbe226f71303c90be412e91f15435f3d3e4fd0
Miles Z. Sterrett <miles@mileszs.com>
2016-10-02 10:25:22 -0400

    Clean up the Plug test a bit

--- test/plugs/authenticate_admin_test.exs
+++ test/plugs/authenticate_admin_test.exs
@@ -11,11 +11,8 @@ defmodule ElixirJobBoard.Plugs.AuthenticateAdminTest do
 
   test "user is redirected when current_user is not assigned" do
     conn = build_conn()
-      |> Map.put(:secret_key_base, String.duplicate("abcdefgh", 8))
-      |> Plug.Session.call(@session)
-      |> fetch_session
-      |> fetch_flash
-      |> ElixirJobBoard.Plugs.AuthenticateAdmin.call(%{})
+            |> setup_session
+            |> ElixirJobBoard.Plugs.AuthenticateAdmin.call(%{})
 
     assert redirected_to(conn) == "/"
   end
@@ -23,12 +20,9 @@ defmodule ElixirJobBoard.Plugs.AuthenticateAdminTest do
   test "user is redirected when current_user is assigned but isn't an admin" do
     user = ElixirJobBoard.Factory.insert(:user, %{ admin: false })
     conn = build_conn()
-      |> Map.put(:secret_key_base, String.duplicate("abcdefgh", 8))
-      |> Plug.Session.call(@session)
-      |> fetch_session
-      |> fetch_flash
-      |> put_session(:current_user, user.id)
-      |> ElixirJobBoard.Plugs.AuthenticateAdmin.call(%{})
+            |> setup_session
+            |> put_session(:current_user, user.id)
+            |> ElixirJobBoard.Plugs.AuthenticateAdmin.call(%{})
 
     assert redirected_to(conn) == "/"
   end
@@ -36,13 +30,18 @@ defmodule ElixirJobBoard.Plugs.AuthenticateAdminTest do
   test "user passes through when current_user is assigned and is an admin" do
     user = ElixirJobBoard.Factory.insert(:user, %{ admin: true })
     conn = build_conn()
+            |> setup_session
+            |> put_session(:current_user, user.id)
+            |> ElixirJobBoard.Plugs.AuthenticateAdmin.call(%{})
+
+    assert conn.status != 302
+  end
+
+  defp setup_session(conn) do
+    conn
       |> Map.put(:secret_key_base, String.duplicate("abcdefgh", 8))
       |> Plug.Session.call(@session)
       |> fetch_session
       |> fetch_flash
-      |> put_session(:current_user, user.id)
-      |> ElixirJobBoard.Plugs.AuthenticateAdmin.call(%{})
-
-    assert conn.status != 302
   end
 end
