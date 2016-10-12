8558a7d70b90b5ffef96b4a4cf870117c2964276
Miles Z. Sterrett <miles@mileszs.com>
2016-10-11 21:25:17 -0400

    Fix posts controller test

--- test/controllers/posts_controller_test.exs
+++ test/controllers/posts_controller_test.exs
@@ -13,15 +13,11 @@ defmodule ElixirJobBoard.PostsControllerTest do
   ])
 
   setup do
-    user = insert(:user)
     conn = build_conn()
-      |> Map.put(:secret_key_base, String.duplicate("abcdefgh", 8))
-      |> Plug.Session.call(@session)
-      |> fetch_session
-      |> fetch_flash
-      |> put_session(:current_user, user.id)
-      |> send_resp(200, "Flusing Session")
-      |> recycle()
+            |> Map.put(:secret_key_base, String.duplicate("abcdefgh", 8))
+            |> Plug.Session.call(@session)
+            |> fetch_session
+            |> fetch_flash
 
     {:ok, conn: conn}
   end
@@ -44,52 +40,61 @@ defmodule ElixirJobBoard.PostsControllerTest do
 
   test "GET /posts/:id/edit", %{conn: conn} do
     job = insert(:job)
-    conn = get conn, "/posts/#{job.id}/edit"
+    user = insert(:user)
+    conn = conn
+            |> assign(:current_user, user)
+            |> get("/posts/#{job.id}/edit")
     assert html_response(conn, 200)
   end
-  #
-  # test "successful POST /posts", %{conn: conn} do
-  #   jobs_count = length(Repo.all(Job))
-  #   job_params = %{"title"        => "Something Important",
-  #                 "description"   => "a new job",
-  #                 "poster_email"  => "poster.email@example.com",
-  #                 "contact_email" => "contact.email@example.com",
-  #                 "location"      => "Somewhere",
-  #                 "published_at"  => Ecto.DateTime.utc}
-  #   conn = post conn, "/posts", %{"job" => job_params}
-  #   assert get_flash(conn, :info) == "Job created successfully."
-  #   assert redirected_to(conn) =~ "/posts"
-  #   assert (length(Repo.all(Job))) > jobs_count
-  #   assert_in_delta(jobs_count, length(Repo.all(Job)), 2)
-  # end
-  #
-  # test "unsuccessful POST /posts", %{conn: conn} do
-  #   jobs_count = length(Repo.all(Job))
-  #   job_params = %{"title"        => "Something Important",
-  #                 "poster_email"  => "poster.email@example.com",
-  #                 "contact_email" => "contact.email@example.com",
-  #                 "location"      => "Somewhere",
-  #                 "published_at"  => Ecto.DateTime.utc}
-  #   refute Job.changeset(%Job{}, job_params).valid?
-  #   conn = post conn, "/posts", %{"job" => job_params}
-  #   assert html_response(conn, 200)
-  #   assert jobs_count == length(Repo.all(Job))
-  #   assert_in_delta(jobs_count, length(Repo.all(Job)), 1)
-  # end
-  #
-  # test "successful PATCH /posts/:id", %{conn: conn} do
-  #   job = insert(:job)
-  #   job_params = %{"title" => "Something Important"}
-  #   conn = patch conn, "/posts/#{job.id}", %{"id" => job.id, "job" => job_params}
-  #   assert get_flash(conn, :info) == "Job successfully updated."
-  #   assert redirected_to(conn) =~ "/posts/#{job.id}"
-  # end
-  #
-  # test "unsuccessful PATCH /posts/:id", %{conn: conn} do
-  #   job = insert(:job)
-  #   job_params = %{"title" => nil}
-  #   conn = patch conn, "/posts/#{job.id}", %{"id" => job.id, "job" => job_params}
-  #   assert html_response(conn, 200)
-  #   refute Job.changeset(job, job_params).valid?
-  # end
+
+  test "successful POST /posts", %{conn: conn} do
+    jobs_count = length(Repo.all(Job))
+    job_params = %{"title"        => "Something Important",
+                  "description"   => "a new job",
+                  "poster_email"  => "poster.email@example.com",
+                  "contact_email" => "contact.email@example.com",
+                  "location"      => "Somewhere",
+                  "published_at"  => Ecto.DateTime.utc}
+    conn = post conn, "/posts", %{"job" => job_params}
+    assert get_flash(conn, :info) == "Job created successfully."
+    assert redirected_to(conn) =~ "/posts"
+    assert (length(Repo.all(Job))) > jobs_count
+    assert_in_delta(jobs_count, length(Repo.all(Job)), 2)
+  end
+
+  test "unsuccessful POST /posts", %{conn: conn} do
+    jobs_count = length(Repo.all(Job))
+    job_params = %{"title"        => "Something Important",
+                  "poster_email"  => "poster.email@example.com",
+                  "contact_email" => "contact.email@example.com",
+                  "location"      => "Somewhere",
+                  "published_at"  => Ecto.DateTime.utc}
+    refute Job.changeset(%Job{}, job_params).valid?
+    conn = post conn, "/posts", %{"job" => job_params}
+    assert html_response(conn, 200)
+    assert jobs_count == length(Repo.all(Job))
+    assert_in_delta(jobs_count, length(Repo.all(Job)), 1)
+  end
+
+  test "successful PATCH /posts/:id", %{conn: conn} do
+    user = insert(:user)
+    job = insert(:job)
+    job_params = %{"title" => "Something Important"}
+    conn = conn
+            |> assign(:current_user, user)
+            |> patch("/posts/#{job.id}", %{"id" => job.id, "job" => job_params})
+    assert get_flash(conn, :info) == "Job successfully updated."
+    assert redirected_to(conn) =~ "/posts/#{job.id}"
+  end
+
+  test "unsuccessful PATCH /posts/:id", %{conn: conn} do
+    user = insert(:user)
+    job = insert(:job)
+    job_params = %{"title" => nil}
+    conn = conn
+            |> assign(:current_user, user)
+            |> patch("/posts/#{job.id}", %{"id" => job.id, "job" => job_params})
+    assert html_response(conn, 200)
+    refute Job.changeset(job, job_params).valid?
+  end
 end
