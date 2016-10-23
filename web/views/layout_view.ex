c5595d2b2f264740c0d141c6e05efe1778f8da82
Miles Z. Sterrett <miles@mileszs.com>
2016-10-23 17:10:45 -0400

    Add Bootstrap 4 alpha in brunch.io

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,20 @@
 defmodule ElixirJobBoard.LayoutView do
   use ElixirJobBoard.Web, :view
+  import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 1, view_module: 1]
+
+  def show_flash(conn) do
+    Phoenix.Controller.get_flash(conn) |> flash_msg
+  end
+
+  def flash_msg(%{"info" => msg}) do
+    ~E"<div class='alert alert-info' role='alert'><p><%= msg %></div>"
+  end
+
+  def flash_msg(%{"error" => msg}) do
+    ~E"<div class='alert alert-danger' role='alert'><%= msg %></div>"
+  end
+
+  def flash_msg(_) do
+    nil
+  end
 end
